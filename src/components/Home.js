f157fe030c26592b1a589f669837dffd705fcbb9
jah1603 <hendersonjamesalexander@gmail.com>
2018-11-19 20:52:32 +0000

    button animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,15 +9,18 @@ var sectionStyle = {
   backgroundSize: 'cover'
 };
 
+
 const Home = () => (
+  <div className='welcome-parent'>
   <div className="welcome-header" style={ sectionStyle }>
     <div className="welcome-message">
       <h1>I'm James</h1>
       <h3>I'm a software developer based in Edinburgh</h3>
       <Link to='/projects'>
-        <button className="button" type="button">Browse my portfolio</button>
+        <button className="button draw-border" type="button">Browse my portfolio</button>
       </Link>
     </div>
+  </div>
   </div>
 );
 
