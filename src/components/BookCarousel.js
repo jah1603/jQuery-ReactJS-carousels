141deb7f63172b67889f82dc759b27caad792cf9
jah1603 <hendersonjamesalexander@gmail.com>
2018-11-22 22:37:24 +0000

    adds inventory tracker carousel

---
+++ src/components/BookCarousel.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Arrow from './Arrow.js'
+import ImageSlide from './ImageSlide.js'
+const imgUrls = ["../images/story1.png", '../images/story2.png', '../images/story3.png', '../images/story4.png'
+]
+
+
+class BookCarousel extends React.Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      currentImageIndex: 0
+    };
+
+    this.nextSlide = this.nextSlide.bind(this);
+    this.previousSlide = this.previousSlide.bind(this);
+  }
+
+  previousSlide () {
+    const lastIndex = imgUrls.length - 1;
+    const { currentImageIndex } = this.state;
+    const shouldResetIndex = currentImageIndex === 0;
+    const index =  shouldResetIndex ? lastIndex : currentImageIndex - 1;
+
+    this.setState({
+      currentImageIndex: index
+    });
+  }
+
+  nextSlide () {
+    const lastIndex = imgUrls.length - 1;
+    const { currentImageIndex } = this.state;
+    const shouldResetIndex = currentImageIndex === lastIndex;
+    const index =  shouldResetIndex ? 0 : currentImageIndex + 1;
+
+    this.setState({
+      currentImageIndex: index
+    });
+  }
+
+  returnImage(index){
+    switch(index) {
+    case 0: return require("../images/story1.png");
+    case 1: return require("../images/story2.png");
+    case 2: return require("../images/story3.png");
+    case 3: return require("../images/story4.png");
+  }
+  }
+
+  render () {
+    return (
+      <div className="carousel">
+
+      <Arrow
+        direction="left"
+        clickFunction={ this.previousSlide }
+        glyph="&#9664;"
+        project="story" />
+
+         <img className="story-screenshot" src={this.returnImage(this.state.currentImageIndex)} ></img>
+
+       <Arrow
+        direction="right"
+        clickFunction={ this.nextSlide }
+        glyph="&#9654;"
+        project='story' />
+
+      </div>
+    );
+  }
+}
+
+export default BookCarousel;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReachCarousel from './ReachCarousel.js';
 import Weather2Carousel from './Weather2Carousel.js';
 import FixtureCarousel from './FixtureCarousel.js';
+import W2WCarousel from './W2WCarousel.js';
+import BookCarousel from './BookCarousel.js';
 
 class Portfolio extends React.Component {
 
@@ -37,7 +39,7 @@ class Portfolio extends React.Component {
 
       <h2 style={{fontWeight: 'normal', fontStyle: 'italic'}}> A modern social networking and dating app built in Python (Django) and ReactJS</h2>
       <h4 className="github"><a href="https://www.github.com/jah1603/Reach--">View on GitHub</a></h4>
-      <p>I undertook this project with two friends. Our vision was a social app for hyperconnected influencers. Together we implemented the code in Python (backend) and ReactJS (frontend), using a PSQL database for data persistence. Reach is a full-stack dating platform that measures users' social reach via web scraping, letting them find, match and interact with other users according to lifestyle preferences, location and age.<br/><br/>
+      <p>I undertook this project with two friends. Our vision was a networking app for hyperconnected social influencers. Together we implemented the code in Python (backend) and ReactJS (frontend), using a PSQL database for data persistence. Reach is a full-stack dating platform that measures users' social reach via web scraping, letting them find, match and interact with other users according to lifestyle preferences, location and age.<br/><br/>
       The app features full JSON web token authentication, geolocation, email activation, and retrieves users' social reach by scraping their follower count on three major social media platforms. All of this underpins a fresh and vibrant UI designed to appeal to millenials.</p>
 
     </div>
@@ -71,7 +73,7 @@ class Portfolio extends React.Component {
       <h4 className="github"><a href="https://play.google.com/store/apps/details?id=com.weather2.weather2walk">View on Google Play store</a></h4>
       <p>Post-CodeClan, I undertook a project with two colleagues involving the creation of a modern dating app for hyperconnected individuals. Together we implemented the code in Python (backend) and ReactJS (frontend), learning lots of new techniques along the way. Reach is a full-stack dating platform that measures users' social reach via web scraping, letting them match with other users according to lifestyle preferences. dates and location. <br/><br/>
       The app uses two APIs (<a href="https://www.skiddle.com/api/">Skiddle</a> and <a href="https://geocode.xyz/">GeoCode</a>) to fetch event and geographical data. Search results are presented on an interactive Leaflet map where users can click through to see more information and buy tickets. The user can also save their favourite events through the use of a MongoDB NoSQL database. JavaScript is used heavily on the front-end to create a modern looking, responsive and dynamic UI.<br/><br/>
-      In addition to learning a huge amount about JavaScript, we also had the opportunity to practise agile methodologies to plan and coordinate as a team. Stand ups, sprints, scrum boards and pair programming were all used to deliver a fully functional MVP with additional features before the deadline.</p>
+      </p>
 
     </div>
 
@@ -116,72 +118,65 @@ class Portfolio extends React.Component {
 </div>
 
 
-    <div className="project-info">
+<div className="project-info">
 
-      <div className="project-paragraph">
-        <h2>Bookseller Profit and Stock Tracker - Ruby</h2>
-        <h4 className="github"><a href="https://www.github.com/gregorcox/Spending-Tracker-Project">GitHub</a></h4>
-        <p>My first solo project at CodeClan involved building a full-stack web application to allow users to track their financial transactions. Users are able to create, update, and delete transactions containing a date, and amount, a merchant and a tag, and view their transactions in a table.<br/><br/>
-        The project was created using Ruby with the Sinatra web framework. A postgreSQL database was also used to store transaction, merchant and tag data from the user. This meant users could also filter their transactions by merchant or tag. The app uses a sleek, minimalist front-end with a subtle colour pallette that results in a user friendly UI.</p>
-      </div>
+<div className="project-paragraph">
 
-      <div className="project-photo">
-        <img className="screenshot" src={require('../images/story1.png')} alt="Invent.story" />
-        <img className="screenshot" src={require('../images/story2.png')} alt="Invent.story" />
-      </div>
+  <table>
+  <tr>
+  <th><img style={{width:'5rem', height: '5rem', marginTop: '0,5rem'}} src={require('../images/books.png')}></img></th>
+  <th>
+  <h2 style={{marginLeft: '0.5rem'}}>
+      Invent.story
+  </h2>
+  </th>
+  </tr>
+  </table>
 
-    </div>
+  <h2 style={{fontWeight: 'normal', fontStyle: 'italic'}}>Bookseller stock and profit tracker built in Ruby using the Sinatra web framework</h2>
+  <h4 className="github"><a href="http://invent-story.herokuapp.com">View on Heroku</a></h4>
+  <p>Another custom-made brief, this was a CodeClan project which aimed to provide information in a novel and engaging way. Weather2Wed presents historical weather summaries for locations throughout Britain and Ireland. Use of the Geograph photography API means that the app benefits from what is effectively a fuzzy search, as any location can be entered as a search term, be it a town, venue, landmark or even a waterfall. <br/><br/>
+</p>
 
-    <div className="project-info">
+</div>
 
-      <div className="project-paragraph">
-        <h2>Weather2... - Mobile App Series Built in React Native</h2>
-          <h4 className="github"><a href="https://github.com/gregorcox/News-Website-Project">GitHub</a></h4>
-        <p>A paired project completed during my time at CodeClan. We were tasked with creating a full-stack web application using Java for a magazine eager to go digital. The app has an editor side, that allows the creation, update, and deletion of articles and journalists. It also has a user side, where users can browse and read articles, view journalists, and share stories on social media.<br/><br/>
-        The app was built using Java and made use of the Spark web framework. A simple database using the Hibernate framework means creating, editing and deleting articles/journalists is quick and easy. New articles appear on the homepage where users can view the latest stories or use the navigation bar to browse articles based on their respective category. Test driven development was used throughout the project to ensure a reliable backend. The front-end uses a clean and user friendly UI that encourages users to make use of all areas of the app.</p>
-      </div>
+<BookCarousel>
 
-      <div className="project-photo">
-        <img className="screenshot" src={require('../images/w2wmob1.png')} alt="Weather2..." />
-        <img className="screenshot" src={require('../images/w2wmob2.png')} alt="Weather2..." />
-        <img className="screenshot" src={require('../images/w2wmob3.png')} alt="Weather2..." />
-        <img className="screenshot" src={require('../images/w2wmob4.png')} alt="Weather2..." />
-      </div>
+</BookCarousel>
 
-    </div>
+
+</div>
+
 
     <div className="project-info">
 
-      <div className="project-paragraph">
-        <h2>FixturePal - Sports Match Scheduler and Results Tracker in Java</h2>
-          <h4 className="github"><a href="https://github.com/gregorcox/News-Website-Project">GitHub</a></h4>
-        <p>A paired project completed during my time at CodeClan. We were tasked with creating a full-stack web application using Java for a magazine eager to go digital. The app has an editor side, that allows the creation, update, and deletion of articles and journalists. It also has a user side, where users can browse and read articles, view journalists, and share stories on social media.<br/><br/>
-        The app was built using Java and made use of the Spark web framework. A simple database using the Hibernate framework means creating, editing and deleting articles/journalists is quick and easy. New articles appear on the homepage where users can view the latest stories or use the navigation bar to browse articles based on their respective category. Test driven development was used throughout the project to ensure a reliable backend. The front-end uses a clean and user friendly UI that encourages users to make use of all areas of the app.</p>
-      </div>
+    <div className="project-paragraph">
 
-      <div className="project-photo">
-        <img className="screenshot" src={require('../images/reach5.png')} alt="Spending Tracker" />
-        <img className="screenshot" src={require('../images/reach6.png')} alt="Event Finder" />
-      </div>
+      <table>
+      <tr>
+      <th><img style={{width:'5rem', height: '5rem', marginTop: '0,5rem'}} src={require('../images/weather2wed.png')}></img></th>
+      <th>
+      <h2 style={{marginLeft: '0.5rem'}}>
+          Weather2Wed (web)
+      </h2>
+      </th>
+      </tr>
+      </table>
 
-    </div>
-
-    <div className="project-info">
-
-      <div className="project-paragraph">
-        <h2>Weather2Wed - Vanilla Javascript Web Application</h2>
-          <h4 className="github"><a href="https://github.com/gregorcox/News-Website-Project">GitHub</a></h4>
-        <p>A paired project completed during my time at CodeClan. We were tasked with creating a full-stack web application using Java for a magazine eager to go digital. The app has an editor side, that allows the creation, update, and deletion of articles and journalists. It also has a user side, where users can browse and read articles, view journalists, and share stories on social media.<br/><br/>
-        The app was built using Java and made use of the Spark web framework. A simple database using the Hibernate framework means creating, editing and deleting articles/journalists is quick and easy. New articles appear on the homepage where users can view the latest stories or use the navigation bar to browse articles based on their respective category. Test driven development was used throughout the project to ensure a reliable backend. The front-end uses a clean and user friendly UI that encourages users to make use of all areas of the app.</p>
-      </div>
-
-      <div className="project-photo">
-        <img className="screenshot" src={require('../images/w2wweb1.png')} alt="Weather2Wed" />
-        <img className="screenshot" src={require('../images/w2wweb2.png')} alt="Weather2Wed" />
-        <img className="screenshot" src={require('../images/w2wweb3.png')} alt="Weather2Wed" />
-      </div>
+      <h2 style={{fontWeight: 'normal', fontStyle: 'italic'}}>A fun wedding day planning tool, vanilla JavaScript</h2>
+      <h4 className="github"><a href="http://weather2wed.herokuapp.com">View on Heroku</a></h4>
+      <p>Another custom-made brief, this was a CodeClan project which aimed to provide information in a novel and engaging way. Weather2Wed presents historical weather summaries for locations throughout Britain and Ireland. Use of the Geograph photography API means that the app benefits from what is effectively a fuzzy search, as any location can be entered as a search term, be it a town, venue, landmark or even a waterfall. <br/><br/>
+      Weather data was sourced from DarkSky using the coordinates obtained from the Geograph API. These same coordinates were also fed into the FourSquare API so as to suggest prospective hotels in and around the searched wedding location. Astronomical equations were also used to predict whether the user's chosen wedding date would fall on a full moon.
+    </p>
 
     </div>
+
+  <W2WCarousel>
+
+  </W2WCarousel>
+
+
+  </div>
   </div>
 )
 }
