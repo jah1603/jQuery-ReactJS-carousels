9c5aecdab50c6ca51116a71febb53417d5ad9629
jah1603 <hendersonjamesalexander@gmail.com>
2018-11-23 23:39:09 +0000

    update descriptions

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,9 @@ const About = () => (
   <div className="about">
     <h1 className="headline">About me</h1>
     <div className="about-paragraph">
-      <p><strong>I am passionate about software development and all things web. I love being able to turn complex problems into great looking user interfaces</strong>. <br/><br/>
+      <p><strong>I am passionate about software development obsessed with the web. I love confronting a challenege head on and turning it into an opportunity for growth and learning.</strong>. <br/><br/>
       I recently graduated from <a href="https://codeclan.com/">CodeClan's</a> software development course in Edinburgh. Throughout the 16 week ct we woulund 8ree full-stack projects (you can check those out <a href="/projects" >here</a>). <br/><br/>
-      I finished the course in September 2018 with a new found passion for software development, particularly using front-end technologies to create to craft incredible websites. I am eager to learn more and continue to seek out new challenges and opportunities.<br/><br/>
+      I finished the course in September 2018 brimming with passion for the industry, equally so for front and back-end and their intersection. I am constantly learning and pushing myself to deepen my knowledge.<br/><br/>
       Before CodeClan I completed a Master of Engineering degree in Naval Architecture, graduating with Merit. The analytical thinking techniques that I learned in my degree remain hugely beneficial when up against a new programming challenge. While I'm not programming I enjoy travelling, playing piano and guitar, learning foreign languages and reading.</p>
     </div>
     <div className="about-photo">
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,12 +3,9 @@ import React from "react";
 const Contact = () => (
   <div className="contact">
     <h1 className="headline">Get in touch</h1>
-    <p className="email">To have a conversation, contact me at <a href= "tel:+447810321157">07810321157</a> or <a href="mailto:hendersonjamesalexander@gmail.com?Subject=Hey%20Gregor!" target="_top"> hendersonjamesalexander@gmail.com</a></p>
-    <p>or find me on:</p>
+    <p className="email">Feel free to contact me on <a href= "tel:+447810321157">07810321157</a> or at <a href="mailto:hendersonjamesalexander@gmail.com?Subject=Hey%20Gregor!" target="_top"> hendersonjamesalexander@gmail.com</a></p>
+    <p>You can also find me on GitHub!</p>
     <div className="social-media">
-        <a href="https://www.linkedin.com/in/jamesahenderson1603/" alt="LinkedIn" target="_blank">
-          <img src={require('../images/linkedin.png')} height="80" width="80" alt="LinkedIn"/>
-        </a>
         <a href="https://www.github.com/jah1603" alt="GitHub" target="_blank">
           <img src={require('../images/github.png')} height="80" width="80" alt="GitHub"/>
         </a>
