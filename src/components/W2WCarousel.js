9eef2d8efe57e848051e82af1e5190dbaa288076
jah1603 <hendersonjamesalexander@gmail.com>
2018-11-25 22:12:15 +0000

    W2W carousel

--- src/components/W2WCarousel.js
+++ src/components/W2WCarousel.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Arrow from './Arrow.js'
 import ImageSlide from './ImageSlide.js'
+import $ from 'jquery';
+import Background from '../images/fast-forward.png'
 const imgUrls = ["../images/w2wweb1.png", '../images/w2wweb2.png',
   '../images/w2wweb3.png', '../images/w2wweb4.png'
 ]
@@ -12,61 +14,65 @@ class W2WCarousel extends React.Component {
     super(props);
 
     this.state = {
-      currentImageIndex: 0
+
     };
 
-    this.nextSlide = this.nextSlide.bind(this);
-    this.previousSlide = this.previousSlide.bind(this);
   }
 
-  previousSlide () {
-    const lastIndex = imgUrls.length - 1;
-    const { currentImageIndex } = this.state;
-    const shouldResetIndex = currentImageIndex === 0;
-    const index =  shouldResetIndex ? lastIndex : currentImageIndex - 1;
+  crossfadeScript(numberOfPhotos){
 
-    this.setState({
-      currentImageIndex: index
-    });
+    $(document).ready(function() {
+
+      var counter = 1;
+
+  $("#cf7-weather2_controls").on('click', 'div', function() {
+    console.log("counter", counter);
+    console.log("numPhotos", numberOfPhotos);
+    $("#cf7-weather2 img").removeClass("opaque");
+
+    // var newImage = $(this).index();
+
+    $("#cf7-weather2 img").eq(counter).addClass("opaque");
+
+    console.log("image", $("#cf7-weather2 img").eq(counter));
+
+    $("#cf7-weather2_controls div").removeClass("selected");
+    $(this).addClass("selected");
+
+    if (counter === numberOfPhotos - 1){
+      counter = 0;
+    }
+
+    else {
+
+    counter++;
+
   }
 
-  nextSlide () {
-    const lastIndex = imgUrls.length - 1;
-    const { currentImageIndex } = this.state;
-    const shouldResetIndex = currentImageIndex === lastIndex;
-    const index =  shouldResetIndex ? 0 : currentImageIndex + 1;
-
-    this.setState({
-      currentImageIndex: index
-    });
-  }
-
-  returnImage(index){
-    switch(index) {
-    case 0: return require("../images/w2wweb1.png");
-    case 1: return require("../images/w2wweb2.png");
-    case 2: return require("../images/w2wweb3.png");
-    case 3: return require('../images/w2wweb4.png');
-  }
+  });
+});
   }
 
   render () {
+
+    this.crossfadeScript(imgUrls.length);
+
     return (
-      <div className="carousel">
 
-      <Arrow
-        direction="left"
-        clickFunction={ this.previousSlide }
-        glyph="&#9664;"
-        project="weather2wed" />
+      <div id="cf7-weather2" className="shadow">
 
-         <img className="weather2wed-screenshot" src={this.returnImage(this.state.currentImageIndex)} ></img>
+      <p id="cf7-weather2_controls" style={{justifyContent: 'center', overflow: 'visible'}}>
+        <div className="pulse" style={{borderRadius: 35, marginTop: '0%', padding: '2%', marginLeft: '28%', height: 8, width: 8, backgroundImage: "url(" + Background + ")"}}>
+        <span className="selected"> </span>
+        </div>
+      </p>
 
-       <Arrow
-        direction="right"
-        clickFunction={ this.nextSlide }
-        glyph="&#9654;"
-        project='weather2wed' />
+
+         <img className="opaque" src={require("../images/w2wweb1.png")} ></img>
+         <img src={require("../images/w2wweb2.png")} ></img>
+         <img src={require("../images/w2wweb3.png")} ></img>
+         <img src={require("../images/w2wweb4.png")} ></img>
+
 
       </div>
     );
