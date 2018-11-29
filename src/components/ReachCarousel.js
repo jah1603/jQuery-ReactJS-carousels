23ce6a4109a0294b219330296d21947f58eed928
jah1603 <hendersonjamesalexander@gmail.com>
2018-11-29 13:19:00 +0000

    modify reach image series

--- src/components/ReachCarousel.js
+++ src/components/ReachCarousel.js
@@ -3,7 +3,7 @@ import Arrow from './Arrow.js'
 import $ from 'jquery';
 import Background from '../images/fast-forward.png'
 
-const reachUrls = ["../images/reach1.png", '../images/reach7.png', '../images/reach8.png', '../images/reach9.png', '../images/reach10.png', '../images/reach11.png', '../images/reach12.png', '../images/reach13.png', '../images/reach14.png', '../images/reach15.png', '../images/reach16.png', '../images/reach17.png', '../images/reach3.png', '../images/reach18.png', '../images/reach19.png']
+const reachUrls = ["../images/reach18.png", "../images/reach1.png", '../images/reach7.png', '../images/reach8.png', '../images/reach9.png', '../images/reach10.png', '../images/reach11.png', '../images/reach12.png', '../images/reach13.png', '../images/reach14.png', '../images/reach15.png', '../images/reach16.png', '../images/reach17.png', '../images/reach3.png', '../images/reach19.png']
 
 
 class ReachCarousel extends React.Component {
@@ -66,7 +66,9 @@ class ReachCarousel extends React.Component {
       </p>
 
 
-         <img className="opaque" src={require("../images/reach1.png")} ></img>
+         <img className="opaque" src={require("../images/reach18.png")} ></img>
+         <img src={require("../images/reach19.png")} ></img>
+         <img src={require("../images/reach1.png")} ></img>
          <img src={require("../images/reach7.png")} ></img>
          <img src={require("../images/reach8.png")} ></img>
          <img src={require("../images/reach9.png")} ></img>
@@ -79,8 +81,6 @@ class ReachCarousel extends React.Component {
          <img src={require("../images/reach16.png")} ></img>
          <img src={require("../images/reach17.png")} ></img>
          <img src={require("../images/reach3.png")} ></img>
-         <img src={require("../images/reach18.png")} ></img>
-         <img src={require("../images/reach19.png")} ></img>
 
       </div>
     );
