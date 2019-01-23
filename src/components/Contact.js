722417e2e15eb1bff055e36c07a4ac638839c1c9
jah1603 <hendersonjamesalexander@gmail.com>
2019-01-23 10:42:27 +0000

    remove telephone number from site

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => (
   <div className="contact">
     <h1 className="headline">Get in touch</h1>
-    <p className="email">Feel free to contact me on <a href= "tel:+447810321157">07810321157</a> or at <a href="mailto:hendersonjamesalexander@gmail.com?Subject=Hi%20James!" target="_top"> hendersonjamesalexander@gmail.com</a></p>
+    <p className="email">Feel free to contact me at <a href="mailto:hendersonjamesalexander@gmail.com?Subject=Hi%20James!" target="_top"> hendersonjamesalexander@gmail.com</a></p>
     <p>You can also find me on GitHub!</p>
     <div className="social-media">
         <a href="https://www.github.com/jah1603" alt="GitHub" target="_blank">
